27ccffd8fc803946d26bc1651fadff8993accd10
ziomega <evaanantonyphilip@gmail.com>
2025-10-09 10:12:07 +0530

    Emulator code

---
+++ palindrome.asm
@@ -0,0 +1,63 @@
+data SEGMENT 
+	MSG1 DB 10,13, 'Enter the string:$'
+	MSG2 DB 10,13, 'Is a Palindrome.$'
+	MSG3 DB 10,13, 'Not a Palindrome.$'
+	
+	STR DB 50 DUP(0)
+data ENDS
+
+code SEGMENT 
+	ASSUME CS: code, DS: data
+	
+	START:
+		MOV AX, DATA
+		MOV DS, AX
+		
+		LEA DX, MSG1
+		MOV AH, 09H
+		INT 21H
+		
+		LEA SI,STR
+		LEA DI,STR
+		MOV AH, 01H
+		
+	NEXT:
+		INT 21H
+		CMP AL,0DH
+		JE TERMINATE
+		MOV [DI],AL
+		INC DI
+		JMP NEXT
+		
+	TERMINATE:
+		MOV AL,'$'
+		MOV [DI],AL
+		DEC DI
+		
+	CHECK_LOOP
+		MOV AL, [SI]
+		CMP AL,[DI]
+		JNE NOTPALINDROME
+		INC SI
+		DEC DI
+		CMP SI,DI
+		JLE CHECK_LOOP
+		
+	PALINDROME:
+		LEA DX, MSG2
+		MOV AH, 09H
+		INT 21H
+		JMP EXIT
+		
+	NOTPALINDROME:
+		LEA DX, MSG3
+		MOV AH, 09H
+		INT 21H
+		
+	EXIT:
+		MOV AH, 4CH
+		INT 21H
+	
+END start 
+code ENDS
+ENDS
